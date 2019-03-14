ed570738a294a9f7b588f4f8904b73a7b1d8c3ba
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-03-14 12:25:33 +0800

    Consul Backup (#7)
    * Initial Consul Backup
    
    * Update charts
    
    * Update README

---
+++ modules/consul_backup/README.md
@@ -0,0 +1,52 @@
+# Consul Backup
+
+This module deploys a
+[Cron Job](https://kubernetes.io/docs/concepts/workloads/controllers/cron-jobs/) in Kubernetes to
+do periodic [snapshots](https://www.consul.io/docs/commands/snapshot.html) of Consul and save it to
+GCS.
+
+## Requirements
+
+- An existing Consul cluster to do the snapshot
+- A Vault server with [Kubernetes Auth Engine](https://www.vaultproject.io/docs/auth/kubernetes.html)
+    configured for the job to authenticate and a
+    [GCP Secrets Engine](https://www.vaultproject.io/docs/secrets/gcp/index.html) mounted.
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| affinity | Affinity for the job | map | `<map>` | no |
+| chart\_name | Helm chart name to provision | string | `"consul-backup-gcs"` | no |
+| chart\_repository | Helm repository for the chart | string | `"amoy"` | no |
+| chart\_version | Version of Chart to install. Set to empty to install the latest version | string | `""` | no |
+| consul\_address | Address of Consul server | string | `"consul-server.service.consul:8500"` | no |
+| env | Additional environment variables for the Ansible playbook | list | `<list>` | no |
+| gcp\_bucket\_name | GCS Storage bucket name | string | n/a | yes |
+| gcp\_bucket\_project | Project where the backup bucket is stored in | string | n/a | yes |
+| gcp\_role\_description | Description for the custom role to allow only creator access to bucket | string | `"Create objects in buckets."` | no |
+| gcp\_role\_id | Name of the custom role to allow only creator access to bucket | string | `"objectCreator"` | no |
+| gcp\_role\_title | Title of the custom role to allow only creator access to bucket | string | `"Bucket Object Creator"` | no |
+| gcp\_secrets\_path | Path to the GCP Secrets Engine in Vault | string | `"gcp"` | no |
+| gcp\_secrets\_roleset | Name of the GCP Secrets Roleset to create | string | `"consul_backup"` | no |
+| gcp\_vault\_role\_description | Description for the custom role to allow Vault to manage bucket IAM permissions | string | `"Allow Vault to manage bucket IAM permissions"` | no |
+| gcp\_vault\_role\_id | Name of the custom role to allow Vault to manage bucket IAM permissions | string | `"vaultBucketIam"` | no |
+| gcp\_vault\_role\_title | Title of the custom role to allow Vault to manage bucket IAM permissions | string | `"Vault Bucket IAM Manager"` | no |
+| gcp\_vault\_service\_account | Service account email for Vault for the GCP secrets engine | string | n/a | yes |
+| gcs\_prefix | Prefix for backup snapshots in the GCS bucket | string | `"backup/consul/"` | no |
+| image | Docker image of the backup job | string | `"basisai/consul-backup-gcs"` | no |
+| kubernetes\_auth\_path | Path to the Kubernetes Auth Engine | string | `"kubernetes"` | no |
+| kubernetes\_auth\_role\_name | Name of the Kubernetes Auth Backend Role | string | `"consul_backup"` | no |
+| namespace | Namespace to run the backup job in | string | `"default"` | no |
+| node\_selector | Node selector for the job | map | `<map>` | no |
+| pull\_policy | Image pull policy of the backup job | string | `"IfNotPresent"` | no |
+| release\_name | Helm release name for Vault | string | `"consul-backup"` | no |
+| schedule | Cron schedule of job in UTC | string | `"0 3 * * *"` | no |
+| service\_account\_name | Name of the service account for the backup job | string | `"consul-backup"` | no |
+| tag | Docker image tag of the backup job | string | `"0.1.0"` | no |
+| tolerations | Tolerations for the job | list | `<list>` | no |
+| ttl\_seconds | TTL of jobs in seconds. Set to an empty string to disable | string | `""` | no |
+| vault\_address | Address for Vault | string | n/a | yes |
+| vault\_ca | PEM encoded Vault CA certificate | string | n/a | yes |
+| vault\_policy | Name of the policy for Vault | string | `"consul_backup"` | no |
+| vault\_ttl | TTL for the Vault Token | string | `"600"` | no |
---
+++ modules/consul_backup/variables.tf
@@ -0,0 +1,164 @@
+variable "release_name" {
+  description = "Helm release name for Vault"
+  default     = "consul-backup"
+}
+
+variable "chart_name" {
+  description = "Helm chart name to provision"
+  default     = "consul-backup-gcs"
+}
+
+variable "chart_repository" {
+  description = "Helm repository for the chart"
+  default     = "amoy"
+}
+
+variable "chart_version" {
+  description = "Version of Chart to install. Set to empty to install the latest version"
+  default     = ""
+}
+
+variable "vault_address" {
+  description = "Address for Vault"
+}
+
+variable "vault_ca" {
+  description = "PEM encoded Vault CA certificate"
+}
+
+variable "gcp_secrets_path" {
+  description = "Path to the GCP Secrets Engine in Vault"
+  default     = "gcp"
+}
+
+variable "gcp_secrets_roleset" {
+  description = "Name of the GCP Secrets Roleset to create"
+  default     = "consul_backup"
+}
+
+variable "gcp_bucket_project" {
+  description = "Project where the backup bucket is stored in"
+}
+
+variable "gcp_bucket_name" {
+  description = "GCS Storage bucket name"
+}
+
+variable "gcs_prefix" {
+  description = "Prefix for backup snapshots in the GCS bucket"
+  default     = "backup/consul/"
+}
+
+variable "gcp_role_id" {
+  description = "Name of the custom role to allow only creator access to bucket"
+  default     = "objectCreator"
+}
+
+variable "gcp_role_title" {
+  description = "Title of the custom role to allow only creator access to bucket"
+  default     = "Bucket Object Creator"
+}
+
+variable "gcp_role_description" {
+  description = "Description for the custom role to allow only creator access to bucket"
+  default     = "Create objects in buckets."
+}
+
+variable "gcp_vault_role_id" {
+  description = "Name of the custom role to allow Vault to manage bucket IAM permissions"
+  default     = "vaultBucketIam"
+}
+
+variable "gcp_vault_role_title" {
+  description = "Title of the custom role to allow Vault to manage bucket IAM permissions"
+  default     = "Vault Bucket IAM Manager"
+}
+
+variable "gcp_vault_role_description" {
+  description = "Description for the custom role to allow Vault to manage bucket IAM permissions"
+  default     = "Allow Vault to manage bucket IAM permissions"
+}
+
+variable "gcp_vault_service_account" {
+  description = "Service account email for Vault for the GCP secrets engine"
+}
+
+variable "kubernetes_auth_path" {
+  description = "Path to the Kubernetes Auth Engine"
+  default     = "kubernetes"
+}
+
+variable "kubernetes_auth_role_name" {
+  description = "Name of the Kubernetes Auth Backend Role"
+  default     = "consul_backup"
+}
+
+variable "service_account_name" {
+  description = "Name of the service account for the backup job"
+  default     = "consul-backup"
+}
+
+variable "namespace" {
+  description = "Namespace to run the backup job in"
+  default     = "default"
+}
+
+variable "vault_ttl" {
+  description = "TTL for the Vault Token"
+  default     = "600"
+}
+
+variable "vault_policy" {
+  description = "Name of the policy for Vault"
+  default     = "consul_backup"
+}
+
+variable "schedule" {
+  description = "Cron schedule of job in UTC"
+  default     = "0 3 * * *"
+}
+
+variable "image" {
+  description = "Docker image of the backup job"
+  default     = "basisai/consul-backup-gcs"
+}
+
+variable "tag" {
+  description = "Docker image tag of the backup job"
+  default     = "0.1.0"
+}
+
+variable "pull_policy" {
+  description = "Image pull policy of the backup job"
+  default     = "IfNotPresent"
+}
+
+variable "consul_address" {
+  description = "Address of Consul server"
+  default     = "consul-server.service.consul:8500"
+}
+
+variable "node_selector" {
+  description = "Node selector for the job"
+  default     = {}
+}
+
+variable "tolerations" {
+  description = "Tolerations for the job"
+  default     = []
+}
+
+variable "affinity" {
+  description = "Affinity for the job"
+  default     = {}
+}
+
+variable "ttl_seconds" {
+  description = "TTL of jobs in seconds. Set to an empty string to disable"
+  default     = ""
+}
+
+variable "env" {
+  description = "Additional environment variables for the Ansible playbook"
+  default     = []
+}
