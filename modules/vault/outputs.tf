39952bd43fa826fb72c6e59436e40e6ce3221d20
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-05-24 10:29:45 +0800

    Output Vault Keyring self link

--- modules/vault/outputs.tf
+++ modules/vault/outputs.tf
@@ -17,3 +17,8 @@ output "gke_service_account_name" {
   description = "Name of the GKE node pool if created"
   value       = "${google_service_account.vault_gke.*.name}"
 }
+
+output "key_ring_self_link" {
+  description = "Self-link of the KMS Keyring created for Vault"
+  value       = "${google_kms_key_ring.vault.self_link}"
+}
