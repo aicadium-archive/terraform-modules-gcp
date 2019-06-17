9be2e0a9d29bc3678be9c01f2dadf50f40c31ed6
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-06-17 15:25:28 +0800

    Rename

--- modules/vault/main.tf
+++ modules/vault/main.tf
@@ -92,10 +92,12 @@ data "template_file" "values" {
     vault_image = var.vault_image
     vault_tag = var.vault_tag
     fullname_override = var.fullname_override
+
     consul_image = var.consul_image
     consul_tag = var.consul_tag
     consul_join = jsonencode(var.consul_join)
     consul_gossip_secret_key_name = jsonencode(var.consul_gossip_secret_key_name)
+
     service_name = var.service_name
     service_type = var.service_type
     service_external_port = var.service_external_port
@@ -104,24 +106,28 @@ data "template_file" "values" {
     service_annotations = jsonencode(var.service_annotations)
     load_balancer_ip = jsonencode(var.load_balancer_ip)
     load_balancer_source_ranges = jsonencode(var.load_balancer_source_ranges)
+
     ingress_enabled = var.ingress_enabled
     ingress_labels = jsonencode(var.ingress_labels)
     ingress_hosts = jsonencode(var.ingress_hosts)
     ingress_annotations = jsonencode(var.ingress_annotations)
     ingress_tls = jsonencode(var.ingress_tls)
+
     cpu_request = var.cpu_request
     memory_request = var.memory_request
     cpu_limit = var.cpu_limit
     memory_limit = var.memory_limit
+
     affinity = jsonencode(var.affinity)
     annotations = jsonencode(var.annotations)
     labels = jsonencode(var.labels)
     pod_annotations = jsonencode(var.pod_annotations)
     tolerations = jsonencode(var.tolerations)
     node_selector = jsonencode(var.node_selector)
-    lifecycle = jsonencode(var.lifecycle)
+    lifecycle = jsonencode(var.container_lifecycle)
     pod_priority_class = var.pod_priority_class
     min_ready_seconds = var.min_ready_seconds
+
     vault_dev = var.vault_dev
     vault_secret_volumes = jsonencode(var.vault_secret_volumes)
     vault_env = jsonencode(concat(local.vault_env, var.vault_env))
