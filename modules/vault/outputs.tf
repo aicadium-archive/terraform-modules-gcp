e930afd2c41403aa70a61d81c882539b5bfbe5e0
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-06-17 14:24:39 +0800

    Upgrade Vault to Terraform 0.12

--- modules/vault/outputs.tf
+++ modules/vault/outputs.tf
@@ -1,24 +1,25 @@
 output "values" {
   description = "Values from the Vault Helm chart"
-  value       = "${helm_release.vault.metadata.0.values}"
+  value       = helm_release.vault.metadata[0].values
 }
 
 output "release_name" {
   description = "Release name of the Helm chart"
-  value       = "${helm_release.vault.metadata.0.name}"
+  value       = helm_release.vault.metadata[0].name
 }
 
 output "gke_service_account" {
   description = "Email ID of the GKE node pool if created"
-  value       = "${google_service_account.vault_gke.*.email}"
+  value       = google_service_account.vault_gke.*.email
 }
 
 output "gke_service_account_name" {
   description = "Name of the GKE node pool if created"
-  value       = "${google_service_account.vault_gke.*.name}"
+  value       = google_service_account.vault_gke.*.name
 }
 
 output "key_ring_self_link" {
   description = "Self-link of the KMS Keyring created for Vault"
-  value       = "${google_kms_key_ring.vault.self_link}"
+  value       = google_kms_key_ring.vault.self_link
 }
+
