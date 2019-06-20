36a99417e0e84f6b168392f0ac871513a324d76d
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-06-20 14:27:18 +0800

    Update remaining modules to Terraform 0.12 (#21)
    * Update CloudSQL backup to Terraform 0,12
    
    * Update Consul Backup
    
    * Fix variables
    
    * Bump image

--- modules/cloudsql_backup/kubernetes_auth.tf
+++ modules/cloudsql_backup/kubernetes_auth.tf
@@ -3,25 +3,25 @@ locals {
 }
 
 resource "vault_kubernetes_auth_backend_role" "job" {
-  backend   = "${var.kubernetes_auth_path}"
-  role_name = "${var.kubernetes_auth_role_name}"
+  backend   = var.kubernetes_auth_path
+  role_name = var.kubernetes_auth_role_name
 
-  bound_service_account_names      = ["${var.service_account_name}"]
-  bound_service_account_namespaces = ["${var.namespace}"]
+  bound_service_account_names      = [var.service_account_name]
+  bound_service_account_namespaces = [var.namespace]
 
-  ttl      = "${var.vault_ttl}"
-  max_ttl  = "${var.vault_ttl}"
-  policies = ["${vault_policy.job.name}"]
+  ttl      = var.vault_ttl
+  max_ttl  = var.vault_ttl
+  policies = [vault_policy.job.name]
 }
 
 data "vault_policy_document" "job" {
   rule {
-    path         = "${local.gcp_secret_path}"
+    path         = local.gcp_secret_path
     capabilities = ["read"]
   }
 }
 
 resource "vault_policy" "job" {
-  name   = "${var.vault_policy}"
-  policy = "${data.vault_policy_document.job.hcl}"
+  name   = var.vault_policy
+  policy = data.vault_policy_document.job.hcl
 }
--- modules/cloudsql_backup/main.tf
+++ modules/cloudsql_backup/main.tf
@@ -1,18 +1,18 @@
 resource "helm_release" "cloudsql_backup" {
-  name       = "${var.release_name}"
-  chart      = "${var.chart_name}"
-  repository = "${data.helm_repository.selected.metadata.0.name}"
-  version    = "${var.chart_version}"
-  namespace  = "${var.namespace}"
+  name       = var.release_name
+  chart      = var.chart_name
+  repository = data.helm_repository.selected.metadata[0].name
+  version    = var.chart_version
+  namespace  = var.namespace
 
   values = [
-    "${data.template_file.values.rendered}",
+    data.template_file.values.rendered,
   ]
 }
 
 data "helm_repository" "selected" {
-  name = "${var.chart_repository}"
-  url  = "${var.chart_repository_url}"
+  name = var.chart_repository
+  url  = var.chart_repository_url
 }
 
 locals {
@@ -27,42 +27,43 @@ gcloud sql backups create \
   --project "${var.cloudsql_project}" \
   --description "Manual Backup at $(date)"
 EOF
+
 }
 
 data "template_file" "values" {
-  template = "${file("${path.module}/templates/values.yaml")}"
+  template = file("${path.module}/templates/values.yaml")
 
-  vars {
-    schedule = "${var.schedule}"
-    env      = "${jsonencode(var.env)}"
+  vars = {
+    schedule = var.schedule
+    env = jsonencode(var.env)
 
-    image       = "${var.image}"
-    tag         = "${var.tag}"
-    pull_policy = "${var.pull_policy}"
+    image = var.image
+    tag = var.tag
+    pull_policy = var.pull_policy
 
-    consul_address = "${var.consul_address}"
+    consul_address = var.consul_address
 
-    node_selector = "${jsonencode(var.node_selector)}"
-    tolerations   = "${jsonencode(var.tolerations)}"
-    affinity      = "${jsonencode(var.affinity)}"
+    node_selector = jsonencode(var.node_selector)
+    tolerations = jsonencode(var.tolerations)
+    affinity = jsonencode(var.affinity)
 
-    vault_address = "${var.vault_address}"
-    vault_ca      = "${jsonencode(var.vault_ca)}"
+    vault_address = var.vault_address
+    vault_ca = jsonencode(var.vault_ca)
 
-    vault_auth_path = "${var.kubernetes_auth_path}"
-    vault_auth_role = "${vault_kubernetes_auth_backend_role.job.role_name}"
-    service_account = "${var.service_account_name}"
+    vault_auth_path = var.kubernetes_auth_path
+    vault_auth_role = vault_kubernetes_auth_backend_role.job.role_name
+    service_account = var.service_account_name
 
-    vault_gcp_path = "${local.gcp_secret_path}"
+    vault_gcp_path = local.gcp_secret_path
 
-    ttl_seconds = "${var.ttl_seconds}"
+    ttl_seconds = var.ttl_seconds
 
-    entrypoint = "${jsonencode(local.entrypoint)}"
+    entrypoint = jsonencode(local.entrypoint)
   }
 }
 
 resource "google_project_service" "cloudsql_admin" {
-  project = "${var.cloudsql_project}"
+  project = var.cloudsql_project
   service = "sqladmin.googleapis.com"
 
   disable_on_destroy = false
