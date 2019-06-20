b278a044f6a852c0d5d8814fee278bf84da77174
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-06-20 13:02:31 +0800

    Update CloudSQL backup to Terraform 0,12

--- modules/cloudsql_backup/gcp_secrets.tf
+++ modules/cloudsql_backup/gcp_secrets.tf
@@ -1,5 +1,4 @@
 # Configure a GCP Secrets Engine Roleset
-
 locals {
   vault_iam_roles = [
     "roles/iam.serviceAccountAdmin",
@@ -9,14 +8,12 @@ locals {
 }
 
 resource "vault_gcp_secret_roleset" "bucket" {
-  depends_on = [
-    "google_project_iam_member.vault",
-  ]
+  depends_on = [google_project_iam_member.vault]
 
-  backend      = "${var.gcp_secrets_path}"
-  roleset      = "${var.gcp_secrets_roleset}"
+  backend      = var.gcp_secrets_path
+  roleset      = var.gcp_secrets_roleset
   secret_type  = "service_account_key"
-  project      = "${var.cloudsql_project}"
+  project      = var.cloudsql_project
   token_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
 
   binding {
@@ -29,22 +26,22 @@ resource "vault_gcp_secret_roleset" "bucket" {
 }
 
 resource "google_project_iam_custom_role" "backup" {
-  role_id     = "${var.gcp_role_id}"
-  title       = "${var.gcp_role_title}"
-  description = "${var.gcp_role_description}"
+  role_id     = var.gcp_role_id
+  title       = var.gcp_role_title
+  description = var.gcp_role_description
 
   permissions = [
     "cloudsql.backupRuns.create",
     "cloudsql.instances.get",
   ]
 
-  project = "${var.cloudsql_project}"
+  project = var.cloudsql_project
 }
 
 resource "google_project_iam_member" "vault" {
-  count = "${length(local.vault_iam_roles)}"
+  count = length(local.vault_iam_roles)
 
-  project = "${var.cloudsql_project}"
-  role    = "${element(local.vault_iam_roles, count.index)}"
+  project = var.cloudsql_project
+  role    = element(local.vault_iam_roles, count.index)
   member  = "serviceAccount:${var.gcp_vault_service_account}"
 }
