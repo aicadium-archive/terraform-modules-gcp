9581bd8712165b10817665bbafa429a4cc4e5741
Chris Sng <chrissng@users.noreply.github.com>
2019-10-07 16:02:42 +0800

    enable or disable function

--- modules/gcp_billing_slack_notify/gcp_secrets.tf
+++ modules/gcp_billing_slack_notify/gcp_secrets.tf
@@ -1,7 +1,11 @@
 data "google_service_account" "notifier" {
+  count = local.enabled
+
   account_id = "${var.billing_dataset_id}-bigquery-reader"
 }
 
 resource "google_service_account_key" "notifier" {
-  service_account_id = "${data.google_service_account.notifier.name}"
+  count = local.enabled
+
+  service_account_id = "${data.google_service_account.notifier[0].name}"
 }
--- modules/gcp_billing_slack_notify/main.tf
+++ modules/gcp_billing_slack_notify/main.tf
@@ -1,21 +1,31 @@
-resource "helm_release" "cloudsql_backup" {
+locals {
+  enabled = var.enabled ? 1 : 0
+}
+
+resource "helm_release" "notifier" {
+  count = local.enabled
+
   name       = var.release_name
   chart      = var.chart_name
-  repository = data.helm_repository.selected.metadata[0].name
+  repository = data.helm_repository.selected[0].metadata[0].name
   version    = var.chart_version
   namespace  = var.namespace
 
   values = [
-    data.template_file.values.rendered,
+    data.template_file.values[0].rendered,
   ]
 }
 
 data "helm_repository" "selected" {
+  count = local.enabled
+
   name = var.chart_repository
   url  = var.chart_repository_url
 }
 
 data "template_file" "values" {
+  count = local.enabled
+
   template = file("${path.module}/templates/values.yaml")
 
   vars = {
