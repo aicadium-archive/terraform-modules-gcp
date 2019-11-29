0480b92cd16b734db39b7d5f56b5e485818291f3
Chris Sng <chrissng@users.noreply.github.com>
2019-11-29 11:12:27 +0800

    Make certmanager enable and bump chart

--- modules/certmanager/iam.tf
+++ modules/certmanager/iam.tf
@@ -1,15 +1,21 @@
 resource "google_service_account" "certmanager" {
+  count = local.certmanager_enabled
+
   account_id   = var.certmanager_service_account
   display_name = "Certmanager Service Account"
   project      = var.project_id
 }
 
 resource "google_project_iam_member" "certmanager" {
-  member  = "serviceAccount:${google_service_account.certmanager.email}"
+  count = local.certmanager_enabled
+
+  member  = "serviceAccount:${google_service_account.certmanager[0].email}"
   role    = "roles/dns.admin"
   project = var.project_id
 }
 
 resource "google_service_account_key" "certmanager" {
-  service_account_id = google_service_account.certmanager.name
+  count = local.certmanager_enabled
+
+  service_account_id = google_service_account.certmanager[0].name
 }
