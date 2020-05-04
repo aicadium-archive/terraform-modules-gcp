dba95061abc53065e7a88ec05ca27aada6069dd4
Yong Wen Chua <lawliet89@users.noreply.github.com>
2020-05-04 13:38:28 +0800

    Fix formatting

--- modules/vault/main.tf
+++ modules/vault/main.tf
@@ -197,7 +197,7 @@ resource "kubernetes_secret" "tls_cert" {
 }
 
 resource "local_file" "values" {
-  count = var.values_file != "" ? 1: 0
-    content  = templatefile("${path.module}/templates/values.yaml", local.chart_values)
-    filename = var.values_file
+  count    = var.values_file != "" ? 1 : 0
+  content  = templatefile("${path.module}/templates/values.yaml", local.chart_values)
+  filename = var.values_file
 }
